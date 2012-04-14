af317bade9af6e0e1507621d04ee49423995577d
Joe Walnes <jwalnes@drwholdings.com>
2012-04-14 17:24:37 -0500

    Show name instead of label in header

--- fritzomatic/component.py
+++ fritzomatic/component.py
@@ -2,8 +2,17 @@ from fritzomatic.format import to_json, to_urltoken
 
 class Component(object):
 
+  def name(self):
+    return self.data['name']
+
   def label(self):
     return self.data['label']
+
+  def description(self):
+    return self.data['description']
+
+  def tags(self):
+    return self.data['tags']
 
   def json(self):
     return to_json(self.data)
